fd1ed73f7532655b20338bdb4a62e840868346e3
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-06-01 19:26:15 -0400

    added description to test contribution

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get("/scrape", function(req, res) {
     });
   
     // Send a message to the client
-    res.send("Scrape Complete");
+    res.send("Scrape Complete. Move on to the home Page.");
     //res.send(data);
     //console.log(data);
   });
